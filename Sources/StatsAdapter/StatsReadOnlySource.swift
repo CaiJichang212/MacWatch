c2ddfa4f1931aec422b5944cfc09f73c247e5076
陆张弛 <li_y_c@qq.com>
2026-06-10 19:05:13 +0800

    feat(StatsReadOnlySource): 添加温度源转换属性
    为StatsReadOnlySource枚举新增temperatureSource计算属性，实现各数据源到TemperatureSource的类型转换

--- Sources/StatsAdapter/StatsReadOnlySource.swift
+++ Sources/StatsAdapter/StatsReadOnlySource.swift
@@ -1,6 +1,23 @@
+import MacWatchCore
+
 public enum StatsReadOnlySource: String, CaseIterable, Sendable {
     case hidSensors
     case smcReadOnly
     case batteryIORegistry
     case nvmeSMART
+}
+
+public extension StatsReadOnlySource {
+    var temperatureSource: TemperatureSource {
+        switch self {
+        case .hidSensors:
+            return .hidSensors
+        case .smcReadOnly:
+            return .smc
+        case .batteryIORegistry:
+            return .batteryIORegistry
+        case .nvmeSMART:
+            return .nvmeSMART
+        }
+    }
 }
